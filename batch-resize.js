50e35fa8118027a98e0c721ef2cc4807833ab5e1
skiprox <seanmichaelscanlan@gmail.com>
2019-03-28 15:09:09 -0400

    Fixing errors

--- batch-resize.js
+++ batch-resize.js
@@ -25,23 +25,28 @@ class BatchResize {
 	 * Resize the images, meaning just make them all the same size
 	 */
 	resize() {
-		let images = jetpack.list(this.basePath).filter(file => {
-			// If the sum of these is greater than -4 at least one of them wasn't -1,
-			// which means we have an image of type jpg jpeg png or gif
-			return (file.indexOf('.jpg') + file.indexOf('.png') + file.indexOf('.gif') + file.indexOf('.jpeg') > -4);
-		});
-		// Loop through the images and use Sharp to resize them
-		images.forEach(image => {
-			sharp(`${this.basePath}/${image}`)
-				.resize(this.width, this.height)
-				.toFile(`${this.basePath}/${this.outputPrefix}${image}`, (err, info) => {
-					if (err) {
-						console.warn(err);
-					} else {
-						console.log(info);
-					}
-				});
-		});
+		console.log(`Looking for images in ${this.basePath}`);
+		if (jetpack.exists(this.basePath)) {
+			let images = jetpack.list(this.basePath).filter(file => {
+				// If the sum of these is greater than -4 at least one of them wasn't -1,
+				// which means we have an image of type jpg jpeg png or gif
+				return (file.indexOf('.jpg') + file.indexOf('.png') + file.indexOf('.gif') + file.indexOf('.jpeg') > -4);
+			});
+			// Loop through the images and use Sharp to resize them
+			images.forEach(image => {
+				sharp(`${this.basePath}/${image}`)
+					.resize(this.width, this.height)
+					.toFile(`${this.basePath}/${this.outputPrefix}${image}`, (err, info) => {
+						if (err) {
+							console.warn(err);
+						} else {
+							console.log(info);
+						}
+					});
+			});
+		} else {
+			console.warn(`The path does not exist on this machine...`);
+		}
 	}
 }
 
